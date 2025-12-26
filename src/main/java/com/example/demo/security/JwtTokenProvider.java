ca877215be742761120f18bcf24bc3eb288a095a
bala-amypo <bala@amypo.in>
2025-12-26 16:50:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -0,0 +1,50 @@
+package com.example.demo.security;
+
+import com.example.demo.model.UserAccount;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtTokenProvider {
+
+    private final String secretKey;
+    private final long validityInMs;
+
+    // constructor used by tests AND Spring bean
+    public JwtTokenProvider(String secretKey, int validityInMs) {
+        this.secretKey = secretKey;
+        this.validityInMs = validityInMs;
+    }
+
+    public String generateToken(UserAccount user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole())
+                .claim("userId", user.getId())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + validityInMs))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUsername(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+}
